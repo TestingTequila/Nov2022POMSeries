20e6716a531e064e9a7dccaaf435d2a42b11513f
Ashish Mishra <ashish.mishra.sse@gmail.com>
2023-06-13 18:17:32 +0530

    Updated as per the Review Comments

--- src/main/java/com/qa/opencart/factory/OrderPage.java
+++ src/main/java/com/qa/opencart/factory/OrderPage.java
@@ -5,8 +5,20 @@ import org.openqa.selenium.By;
 public class OrderPage {
 
     By orderId = By.id("oder");
+    By priceId = By.id("oder");
+    By productId = By.id("oder");
     public  void getOrder()
     {
         System.out.println(this.orderId);
+    }
+
+    public  void getPrice()
+    {
+        System.out.println(this.priceId);
+    }
+
+    public  void getProduct()
+    {
+        System.out.println(this.productId);
     }
 }
